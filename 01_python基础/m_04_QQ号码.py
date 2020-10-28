fea77a0bd62f6ee00e5ae18dffd63e053cdeb798
jinzhe-mu <1421827450@qq.com>
2020-10-28 17:16:34 +0800

    变量定义-qq号码

---
+++ 01_python基础/m_04_QQ号码.py
@@ -0,0 +1,9 @@
+# 1、定义一个变量记录QQ号码
+qq_number = 'mumumumumu'
+
+# 2、定义一个变量记录QQ密码
+qq_password = '123456'
+
+# 如果希望通过解释器的方式，输出变量的内容，需要使用print函数
+print("账户：{}".format(qq_number))
+print("密码：{}".format(qq_password))
