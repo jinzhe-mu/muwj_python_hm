7ef53fde3d3870a2a80c6bb4dbf6a9760deedae0
jinzhe-mu <1421827450@qq.com>
2020-11-12 16:50:02 +0800

    参数使用

---
+++ 04_函数/m_05_函数的参数.py
@@ -0,0 +1,13 @@
+def sum_2_num(num1, num2):
+    """计算两个数字的求和"""
+    """
+    在函数名的后面的小括号内部填写 参数
+    多个参数之间使用 , 分隔
+    """
+    # num1 = 10
+    # num2 = 20
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+
+
+sum_2_num(10, 20)
