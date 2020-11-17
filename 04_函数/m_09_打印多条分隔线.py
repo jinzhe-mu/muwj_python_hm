3b395e35548a8488499a1e1c8772e46d238d0d94
jinzhe-mu <1421827450@qq.com>
2020-11-17 10:54:21 +0800

    函数调用函数打印分隔符（自动传参）+添加多行函数注释

--- 04_函数/m_09_打印多条分隔线.py
+++ 04_函数/m_09_打印多条分隔线.py
@@ -6,17 +6,23 @@
 """
 
 
-def print_line3(char3, times3):
+def print_line3(char, times):
     """控制打印的字符，次数"""
-    print(char3 * times3)
+    print(char * times)
 
 
-def print_line4(cow):  # 控制打印的行数
-    """调用需求3完成需求4"""
+def print_line4(char, times, cow):  # cow控制打印的行数
+    """通过调用需求3完成需求4打印分隔线
+
+    :param char: 分隔线使用的风格字符
+    :param times: 分隔线重复的次数
+    :param cow: 分隔线的行数
+    """
+    #  光标停留在函数上，等待一下出现黄色小灯泡，选择“Add Type..."选择，可以自动增加对参数解释的注释
     col = 0
     while col < cow:
-        print_line3("m", 50)
+        print_line3(char, times)
         col += 1
 
 
-print_line4(5)
+print_line4("2", 20, 5)  # 选择函数后按ctrl+Q可以查看对函数的注释
