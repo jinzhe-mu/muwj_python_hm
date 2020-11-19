cb910ae10408303a0b2563f0698e456c37f38294
jinzhe-mu <1421827450@qq.com>
2020-11-19 15:44:17 +0800

    格式化字符串与元祖组合使用 列表元祖互转

--- 05_高级数据类型/m_08_格式化字符串.py
+++ 05_高级数据类型/m_08_格式化字符串.py
@@ -12,13 +12,13 @@ print("直接输出字符串：", info_str)
 
 print()
 
-# 元祖转列表
+# 元祖转列表 list(元祖)
 print("元祖：", info_tuple)
 print("元祖转列表：", list(info_tuple))
 
 print()
 
-# 列表转元祖
+# 列表转元祖 tuple(列表)
 print("列表：", info_list)
 print("列表转元祖：", tuple(info_list))
 
