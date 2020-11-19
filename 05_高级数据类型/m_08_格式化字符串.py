fff81bfbf6b24172f0a9b8be5dd8b3d5bb91101a
jinzhe-mu <1421827450@qq.com>
2020-11-19 15:38:18 +0800

    格式化字符串与元祖组合使用 列表元祖互转

---
+++ 05_高级数据类型/m_08_格式化字符串.py
@@ -0,0 +1,24 @@
+info_tuple = ("小张", 21, 180)
+info_list = [5, 8, "zhangsan"]
+
+# 格式化字符串后面的 ‘()’ 本质上是元祖
+
+print("%s 年龄是 %d 身高是 %.2f" % ("小明", 18, 185))
+
+print("%s 年龄是 %d 身高是 %.2f" % info_tuple)
+
+info_str = "%s 年龄是 %d 身高是 %.2f" % info_tuple
+print("直接输出字符串：", info_str)
+
+print()
+
+# 元祖转列表
+print("元祖：", info_tuple)
+print("元祖转列表：", list(info_tuple))
+
+print()
+
+# 列表转元祖
+print("列表：", info_list)
+print("列表转元祖：", tuple(info_list))
+
