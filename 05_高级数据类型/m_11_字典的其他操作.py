0dc4fe2b425f60f8bae1262424b8cde0895f527b
jinzhe-mu <1421827450@qq.com>
2020-11-20 10:06:55 +0800

    字典的统计合并清空

--- 05_高级数据类型/m_11_字典的其他操作.py
+++ 05_高级数据类型/m_11_字典的其他操作.py
@@ -1,10 +1,20 @@
-import unittest
+xiaoming_dict = {"name": "小明",
+                 "gender": True}
+
+# 1、统计键值对数量
+print("字典的长度：", len(xiaoming_dict))
+
+# 2、合并字典
+temp_dict = {"age": 18,
+             "height": 175,
+             "gender": False}
+
+# 注意：如果被合并的字典中包含意见存在的键值对，会覆盖原有键值对
+xiaoming_dict.update(temp_dict)
+print("合并两个字典：", xiaoming_dict)
+
+# 3\清空字典
+xiaoming_dict.clear()
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+print("清空字典：", xiaoming_dict)
