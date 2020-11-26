13b90d86beee8aae39d9cff62ff4031b50f7af23
jinzhe-mu <1421827450@qq.com>
2020-11-26 10:47:25 +0800

    字符串文本对齐+去除空白字符

--- 05_高级数据类型/m_18_字符串文本对齐.py
+++ 05_高级数据类型/m_18_字符串文本对齐.py
@@ -11,6 +11,13 @@ poem = ["登鹳雀楼",
         "黄河入海流",
         "欲穷千里目",
         "更上一层楼"]
+
+poem2 = [" 登鹳 雀楼 \t\n",
+         " \t\n   王之涣  ",
+         "   白日依山尽 ",
+         " 黄河入海流 ",
+         " 欲穷千里目 ",
+         " 更上一层楼 "]
 # i = 0
 # while i < len(poem):
 #     print(poem[i].center(10))
@@ -30,3 +37,10 @@ for poem_str in poem:
     # 当英文空格不能达到要求时，切换为中文半角状态下，加入空格参数
     print("|%s|" % poem_str.rjust(10, "　"))
 
+
+# 2、字符串去除空白字符
+
+# 先使用strip方法去除字符串中的空白字符
+# 再使用center方法使字符串居中显示文本
+for poem_str2 in poem2:
+    print("|%s|" % poem_str2.strip().center(10, "　"))
