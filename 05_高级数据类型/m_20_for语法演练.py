df1d4fc9e8ca443512f9fad7876314d1f06cf8d9
jinzhe-mu <1421827450@qq.com>
2020-12-07 16:33:54 +0800

    for循环与else搭配使用

--- 05_高级数据类型/m_20_for语法演练.py
+++ 05_高级数据类型/m_20_for语法演练.py
@@ -12,7 +12,7 @@ else:
 如果 存在，提示并且退出循环
 如果 不存在，在 循环整体结束 后，希望 得到一个统一的提示
 """
-# student_list = [{"name": "小金", "eag": 18, "gender": True, "weight": 56, "height": 160},
+# students = [{"name": "小金", "eag": 18, "gender": True, "weight": 56, "height": 160},
 #                 {"name": "小名", "eag": 19, "gender": False, "weight": 62, "height": 180},
 #                 {"name": "小雪", "eag": 17, "gender": True, "weight": 50, "height": 156},
 #                 {"name": "小沐", "eag": 16, "gender": True, "weight": 52, "height": 171},
@@ -20,15 +20,17 @@ else:
 #                 ]
 #
 # find_name = input("请输入查找人姓名：")
-# for student_dic in student_list:
 #
-#     # 循环所有字典进行查找
-#     # 判断当循环到的name键值是否等于等于find_name
-#     if student_dic["name"] == find_name:
-#         # 循环字典的key值
-#         for student_str in student_dic:
-#             # 等于时将这个字典里面的内容全部输出，且不换行
-#             print(f"{student_str}是：{student_dic[student_str]}", end=" \t")
+# for stu_dict in students:
+#     print(stu_dict)
+#     # 判断当前遍历的字典中姓名是否为find_name
+#     if stu_dict["name"] == find_name:
+#         print("找到了")
+#         # 如果已经找到，直接退出循环，就不需要再对后续的数据进行比较
+#         break
+# else:
+#     print("没有找到")
+# print("循环结束")
 
 
 student_list = [{"name": "小金", "eag": 18, "gender": True, "weight": 56, "height": 160},
